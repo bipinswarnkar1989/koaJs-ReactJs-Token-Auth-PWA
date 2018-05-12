707a195ce5b23c084fb62ce32b2fd525137d0d0d
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2018-05-12 12:29:53 +0530

    Basic PWA is ready

--- user-react-redux-client/public/service-worker.js
+++ user-react-redux-client/public/service-worker.js
@@ -1,29 +1,56 @@
+var CACHE_VERSION = 'react-pwa-v1';
+
 self.addEventListener('install', function(event) {
-  console.log('[Service Worker] Installing Service Worker ...', event);
+  
+  
   event.waitUntil(
-    caches.open('static').then(function(cache) {
-      cache.addAll(['/', '/index.html', '/app.js', '/manifest.json']);
-    })
+    caches.open(CACHE_VERSION)
+      .then(function(cache) {
+        // The asset-manifest.json contains the names of assets to cache 
+        fetch("asset-manifest.json")
+          .then(response => {
+            return response.json()
+          })
+          .then(assets => {
+          
+            cache.addAll(
+              [
+                "/",
+                assets["main.js"],
+                assets["main.css"],
+                assets["static/media/logo.svg"]
+              ]
+            );
+          })
+      })
   );
-});
 
-self.addEventListener('activate', function(event) {
-  console.log('[Service Worker] Activating Service Worker ....', event);
 });
-
+// Serves the cached response for the corresponding request
 self.addEventListener('fetch', function(event) {
+  
   event.respondWith(
-    caches.match(event.request).then(function(response) {
-      if (response) {
-        return response;
-      } else {
-        return fetch(event.request).then(function(res) {
-          return caches.open('dynamic').then(function(cache) {
-            cache.put(event.request.url, res.clone());
-            return res;
-          });
-        });
-      }
-    })
+        caches.match(event.request).then(function(response) {
+            if(response) return response;
+            return fetch(event.request);
+        })
   );
+  
+});
+
+// Delete old cach different from the current version
+self.addEventListener("activate", event => {
+
+
+event.waitUntil(
+  caches.keys()
+    .then(keys =>
+      Promise.all(keys.map(key => {
+        if (key !== CACHE_VERSION) {
+         
+          return caches.delete(key);
+        }
+      }))
+    )
+);
 });
