010811407b564de82ad1e3e83273c998f391000b
bipinswarnkar1989 <vipinswarnkar1989@gmail.com>
2018-06-01 23:42:40 +0530

    Added saga for Google Places Autocomplete api request

--- user-koajs-server/src/controllers/user/index.js
+++ user-koajs-server/src/controllers/user/index.js
@@ -2,5 +2,9 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 class userController {
-
+    register = async () => {
+        
+    }
 }
+
+export default userController;
